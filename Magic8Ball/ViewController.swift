67deeea311fd87138caa72eddfe9193af0963889
Betterteng <betterteng@gmail.com>
2017-11-06 11:34:36 +1100

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by 滕施男 on 6/11/17.
+//  Copyright © 2017 Shinan Teng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
